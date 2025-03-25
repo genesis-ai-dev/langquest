567b7977401ed0ae89dd4ae3fa50ba6ba02b0489
keeandev <38674879+keeandev@users.noreply.github.com>
2025-03-25 10:29:39 -0500

    feat: enhance app configuration for multiple environments
    - Added functions to dynamically set app name and scheme based on APP_VARIANT in app.config.ts.
    - Updated eas.json to define environment variables for development, preview, and production builds, improving build configuration management.

--- app.config.ts
+++ app.config.ts
@@ -3,15 +3,41 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 const projectId = 'fafd03a9-a42c-44c7-849c-b0f84fbffe93';
 const iconPath = './assets/images/langquest_icon_v1.png';
 
+const getAppName = () => {
+  switch (process.env.APP_VARIANT) {
+    case 'development':
+      return 'LangQuest (Dev)';
+    case 'preview':
+      return 'LangQuest (Preview)';
+    default:
+      return 'LangQuest';
+  }
+};
+
+const getEnvironmentSuffix = () => {
+  switch (process.env.APP_VARIANT) {
+    case 'development':
+      return 'dev';
+    case 'preview':
+      return 'prev';
+    default:
+      return '';
+  }
+};
+
+const appSlug = 'langquest';
+const appPackage = `com.etengenesis.${appSlug}.${getEnvironmentSuffix()}`;
+const appScheme = `langquest-${getEnvironmentSuffix()}`;
+
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   owner: 'eten-genesis',
-  name: 'LangQuest',
-  slug: 'langquest',
+  name: getAppName(),
+  slug: appSlug,
   version: '1.0.0',
   orientation: 'portrait',
   icon: iconPath,
-  scheme: 'langquest',
+  scheme: appScheme,
   userInterfaceStyle: 'automatic',
   splash: {
     image: './assets/images/splash.png',
@@ -20,14 +46,14 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   ios: {
     supportsTablet: true,
-    bundleIdentifier: 'com.etengenesis.langquest'
+    bundleIdentifier: appPackage
   },
   android: {
     adaptiveIcon: {
       foregroundImage: './assets/images/langquest_icon_v1.png',
       backgroundColor: '#ffffff'
     },
-    package: 'com.etengenesis.langquest',
+    package: appPackage,
     intentFilters: [
       {
         action: 'VIEW',
@@ -79,6 +105,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     url: `https://u.expo.dev/${projectId}`
   },
   runtimeVersion: {
-    policy: 'appVersion'
+    policy: 'nativeVersion'
   }
 });
