a714b8d6008d29ebcc9d81ebaacb14a9adff9cbd
keeandev <38674879+keeandev@users.noreply.github.com>
2025-08-01 19:56:47 -0500

    Enhance useExpoUpdates hook to manage update status
    - Added state management for update status with useState.
    - Implemented useEffect to log the current update status.
    - Updated checkForUpdate function to set status during checking, downloading, and reloading phases.
    - Added error handling to set status for restarting the app on download failure.

--- hooks/useExpoUpdates.ts
+++ hooks/useExpoUpdates.ts
@@ -1,10 +1,19 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import * as Expo from 'expo';
 import * as Updates from 'expo-updates';
+import { useEffect, useState } from 'react';
 import { useNetworkStatus } from './useNetworkStatus';
 
 export function useExpoUpdates() {
   const queryClient = useQueryClient();
   const isConnected = useNetworkStatus();
+  const [status, setStatus] = useState<
+    'idle' | 'checking' | 'downloading' | 'reloading' | 'restarting'
+  >('idle');
+
+  useEffect(() => {
+    console.log('update status', status);
+  }, [status]);
 
   const {
     data: updateInfo,
@@ -14,6 +23,7 @@ export function useExpoUpdates() {
   } = useQuery({
     queryKey: ['updates'],
     queryFn: async () => {
+      setStatus('checking');
       const { isAvailable, ...update } = await Updates.checkForUpdateAsync();
       return {
         ...update,
@@ -32,16 +42,24 @@ export function useExpoUpdates() {
       if (!updateInfo?.isUpdateAvailable) {
         throw new Error('No update available');
       }
+      setStatus('downloading');
       await Updates.fetchUpdateAsync();
     },
     onSuccess: async () => {
-      await Updates.reloadAsync();
+      setStatus('reloading');
       void queryClient.invalidateQueries({ queryKey: ['updates'] });
+      await Updates.reloadAsync(); // reload without restarting app
+    },
+    onError: async (error) => {
+      console.error('Error downloading update', error);
+      setStatus('restarting');
+      await Expo.reloadAppAsync(); // restart app entirely
     }
   });
 
   return {
     updateInfo,
+    status,
     isLoading,
     checkForUpdate,
     downloadUpdate: downloadUpdateMutation.mutateAsync,
