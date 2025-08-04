6b2e81f7dbb7d452b862e36ca0373922e40e097d
Rafael Winter <rafaelwinter01@gmail.com>
2025-08-04 08:32:30 -0700

    Fixing redirect link when invalid credentials
    Added link to navigate to Register page when user put invalid credentials on Sign-In.
    Added navigation feature to keep email already provided among all Authentication view.
    Added navigation feature to prevent flickering between pages

--- navigators/AuthNavigator.tsx
+++ navigators/AuthNavigator.tsx
@@ -5,17 +5,55 @@ import React, { useState } from 'react';
 
 export type AuthView = 'sign-in' | 'register' | 'forgot-password';
 
+export interface SharedAuthInfo {
+  email?: string;
+}
+
 export function AuthNavigator() {
   const [currentView, setCurrentView] = useState<AuthView>('sign-in');
+  const [sharedAuthInfo, setSharedAuthInfo] = useState<SharedAuthInfo>({});
+
+  function handleNavigation(view: AuthView, sharedAuthInfo?: SharedAuthInfo) {
+    setCurrentView(view);
+    if (sharedAuthInfo) {
+      setSharedAuthInfo(sharedAuthInfo);
+    } else {
+      setSharedAuthInfo({});
+    }
+  }
 
   switch (currentView) {
     case 'sign-in':
-      return <SignInView2 onNavigate={setCurrentView} />;
+      return (
+        <SignInView2
+          key="sign-in"
+          onNavigate={handleNavigation}
+          sharedAuthInfo={sharedAuthInfo}
+        />
+      );
     case 'register':
-      return <RegisterView2 onNavigate={setCurrentView} />;
+      return (
+        <RegisterView2
+          key="register"
+          onNavigate={handleNavigation}
+          sharedAuthInfo={sharedAuthInfo}
+        />
+      );
     case 'forgot-password':
-      return <ForgotPasswordView2 onNavigate={setCurrentView} />;
+      return (
+        <ForgotPasswordView2
+          key="forgot"
+          onNavigate={handleNavigation}
+          sharedAuthInfo={sharedAuthInfo}
+        />
+      );
     default:
-      return <SignInView2 onNavigate={setCurrentView} />;
+      return (
+        <SignInView2
+          key="sign-in"
+          onNavigate={handleNavigation}
+          sharedAuthInfo={sharedAuthInfo}
+        />
+      );
   }
 }
