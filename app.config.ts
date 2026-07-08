083974f1f41e7e053dd78bd7097b10c0fef5bdff
CalJosKos <120157396+CalJosKos@users.noreply.github.com>
2026-07-08 13:09:19 -0700

    Bump app version, clean patch

--- app.config.ts
+++ app.config.ts
@@ -122,7 +122,7 @@ export default ({ config }: ConfigContext): ExpoConfig =>
     owner: 'eten-genesis',
     name: getAppName(appVariant),
     slug: 'langquest',
-    version: '2.2.2',
+    version: '2.2.3',
     orientation: 'portrait',
     icon: iconLight,
     scheme: getScheme(appVariant),
