5e621b0b2772f7dca20797d9baefbb3d48e20e92
CalJosKos <120157396+CalJosKos@users.noreply.github.com>
2026-07-13 15:36:48 -0700

    Remove accidental extra version bump

--- app.config.ts
+++ app.config.ts
@@ -182,7 +182,7 @@ export default ({ config }: ConfigContext): ExpoConfig =>
     owner: 'eten-genesis',
     name: getAppName(appVariant),
     slug: 'langquest',
-    version: '2.2.3',
+    version: '2.2.2',
     orientation: 'portrait',
     icon: iconLight,
     scheme: getScheme(appVariant),
