19762a04ce8ec78e43f3427452470fb065297578
CalJosKos <120157396+CalJosKos@users.noreply.github.com>
2026-07-13 20:16:13 -0700

    Bump version, remove posthog identify() (#906)
    - Bump version higher so we avoid decoder key sourcemap conflicts with posthog
    - Deactivate identify() for posthog (we still need to sort out GDPR stuff)

--- app.config.ts
+++ app.config.ts
@@ -218,7 +218,7 @@ export default ({ config }: ConfigContext): ExpoConfig =>
     owner: 'eten-genesis',
     name: getAppName(appVariant),
     slug: 'langquest',
-    version: '2.2.2',
+    version: '2.2.4',
     orientation: 'portrait',
     icon: iconLight,
     scheme: getScheme(appVariant),
